f39cadbd8538b5229096d7c046fff4d8dba41818
Muneyuki Noguchi <nogu.dev@gmail.com>
2020-10-25 21:00:54 +0900

    Add a sample code

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -1,20 +1,3 @@
-# go-calculator
-
-## Usage
-
-```
-$ go run cmd/calculator/main.go
-calculator> (2.5 - 1.35) * 2.0
-2.3
-calculator> -sin((-1+2.5)*pi)
-1
-calculator> 180*atan2(log(e), log10(10))/pi
-45
-calculator> exit
-```
-
-You can also use the `calculator.Calculate()` in your application:
-```go
 package main
 
 import (
@@ -42,10 +25,4 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(val) // 45
-
 }
-```
-
-## References
-
-* [chibicc](https://github.com/rui314/chibicc): A small C compiler
